d14e38a6b3da807a4bfc4aa793aa1f729e5a5a4a
Kevin McIntosh <KevJames3@gmail.com>
2014-05-09 12:49:47 -0700

    Added base options for a car.  Added different types of cars.  Added the demo method

--- Session03_AbstractFactory_Decorator/Example04_Ruby_Decorator.rb
+++ Session03_AbstractFactory_Decorator/Example04_Ruby_Decorator.rb
@@ -26,25 +26,86 @@ module AbstractInterface
   end
 end
 
-module InitializeCar
-  def initialize(name)
-    @name = name
-  end
-end
-
 class Car
   include AbstractInterface
-  attr_reader(:name, :description, :cost)
+
+  @@globalOptions = {
+    :wheels => {:cost => 12000.00, :desc => "4 wheels"},
+    :doors => {:cost => 2000.00, :desc => "2 doors"},
+    :ac => {:cost => 1500.00, :desc => "AC"},
+    :sound => {:cost => 1000.00, :desc => "premium sound system"},
+    :navigation => {:cost => 2000.00, :desc => "navigation"},
+    :automatic_transmission => {:cost => 3000.00, :desc => "automatic transmission"},
+    :manual_transmission => {:cost => 2500.00, :desc => "manual transmission"},
+    :v8 => {:cost => 6000.00, :desc => "V8"},
+    :super_charger => {:cost => 3000.00, :desc => "super-charger"}
+  }
+
+  def cost
+    cost = 0.0
+    @options.each do |option|
+      cost += @@globalOptions[option][:cost]
+    end
+
+    return cost
+  end
+
+  def description
+    description = "#{@name} with "
+    listOfDescriptions = []
+    options.each do |option|
+      listOfDescriptions.push(@@globalOptions[option])
+    end
+
+    description = description + listOfDescriptions.join(",")
+    return description
+  end
 end 
 
 ########################
 # Classes
 ########################
 
+class RunAbout < Car
+  def initialize()
+    @name = "RunAbout"
+    @options = [:wheels, :doors]
+  end
+end
+
+class SUV < Car
+  def initialize()
+    @name = "SUV"
+    @options = [:wheels, :doors, :ac, :automatic_transmission]
+  end
+end
+
+class Status < Car
+  def initialize()
+    @name = "Status"
+    @options = [:wheels, :doors, :ac, :sound, 
+      :navigation, :automatic_transmission]
+  end
+end
+
+class Performance < Car
+  def initialize()
+    @name = "Performance"
+    @options = [:wheels, :doors, :ac, :sound, 
+      :navigation, :manual_transmission, :v8, :super_scharger]
+  end
+end
+
 ########################
 # Methods
 ########################
 
+def demo()
+  puts "<< Hello Decorator Problem >>"
+end
+
 ########################
 # Main
 ########################
+
+demo()
