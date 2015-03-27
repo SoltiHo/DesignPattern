f1b9942a0b6d54a83b74ea2da1f50bed2c6a7013
Chia-Chiunn Ho (Solti) <Solti.Ho@Gmail.com>
2015-03-26 21:51:51 -0700

    sync Observer

---
+++ DesignPatternLabs/Observer/MyObserver.cpp
@@ -0,0 +1,132 @@
+#include "lab4_problem_observer.h"
+#include <set>
+#include <vector>
+
+namespace observer {
+    // ---------------------------------------------
+    class Observer {
+    public:
+        virtual void udpate() = 0;
+    };
+
+    class ThingAdapter : public Observer {
+    public:
+        ThingAdapter(string name) : _thing(name){};
+        virtual void udpate(){ _thing.makesMischief(); }
+    private:
+        homework::observer_problem::Thing _thing;
+    };
+
+    class FishAdapter : public Observer {
+    public:
+        FishAdapter(){};
+        virtual void udpate(){ _fish.complains(); }
+    private:
+        homework::observer_problem::Fish _fish;
+    };
+
+    class ChildAdapter : public Observer {
+    public:
+        ChildAdapter(string gender) : _child(gender){};
+        virtual void udpate(){ _child.hasFun(); }
+    private:
+        homework::observer_problem::Child _child;
+    };
+
+    class MomAdapter : public Observer {
+    public:
+        MomAdapter(){};
+        virtual void udpate(){ _mom.returns(); }
+    private:
+        homework::observer_problem::Mom _mom;
+    };
+
+    // ---------------------------------------------
+    class Subject {
+    public:
+        void attach(Observer* pObj){
+            subscribers.insert(pObj);
+        }
+        void detach(Observer* pObj){
+            subscribers.erase(pObj);
+        }
+
+        
+    protected:
+        void notify(){
+            for (auto& pObj : subscribers){
+                pObj->udpate();
+            }
+        }
+    private:
+        set<Observer*> subscribers;
+    };
+
+    class CatInTheHat : public Subject{
+    public:
+        void say(string words){
+            cout << "  Cat in the Hat says \"" << words << "\"." << endl;
+            this->notify();
+        }
+        ~CatInTheHat() {
+            cout << "  Cat in the hat left: ~Subject.\n";
+            cout << endl;
+        }
+    };
+
+    void demo() {
+        CatInTheHat c;
+        ThingAdapter* thing1 = new ThingAdapter("1");
+        ThingAdapter* thing2 = new ThingAdapter("2");
+        FishAdapter*  fish = new FishAdapter;
+        ChildAdapter* boy = new ChildAdapter("Boy");
+        ChildAdapter* girl = new ChildAdapter("Girl");
+        MomAdapter*	  mom = new MomAdapter;
+
+        // Event 1.
+        c.attach(thing1);
+        c.attach(thing2);
+        c.attach(boy);
+        c.attach(girl);
+        c.say("Hello");
+        cout << endl;
+
+        // Event 2.
+        c.detach(thing2);
+        delete thing2;
+        c.say("let's play");
+        cout << endl;
+
+        // Event 3.
+        c.attach(fish);
+        c.say("rock & roll");
+        cout << endl;
+
+        // Cleanup 1.
+        c.detach(thing1);
+        c.detach(fish);
+        c.detach(girl);
+        c.detach(boy);
+        delete thing1;
+        delete fish;
+        delete girl;
+        delete boy;
+
+        // Event 4.
+        c.attach(mom);
+        c.say("Goodbye");
+        cout << endl;
+
+        delete mom;
+    }
+}
+int main(){
+    std::cout << "------ observer_problem demo ------" << std::endl;
+    homework::observer_problem::demo();
+    std::cout << std::endl << std::endl;
+
+    std::cout << "------ observer demo ------" << std::endl;
+    observer::demo();
+    std::cout << std::endl << std::endl;
+
+}
--- DesignPatternLabs/Observer/lab4_problem_observer.h
+++ DesignPatternLabs/Observer/lab4_problem_observer.h
@@ -19,126 +19,128 @@
 #define OBSERVER_H_
 
 #include <list>
+#include <string>
+#include <iostream>
 using namespace std;
 
 namespace homework {
 
-/* Consider the Dr. Seuss children's story, "The Cat in the Hat."
- * When the cat in the hat says something, the things, children & fish respond.
- * In a procedural approach, the Subject class (the cat in the hat) has to know
- * about all the other classes. When they change, Subject has to be recompiled.
- * In addition, Subject is managing multiple objects, of multiple classes, with
- * multiple interfaces, which leads to long, hard to follow code.
- * Use the Observer pattern to decouple the Subject class from the others.
- */
+    /* Consider the Dr. Seuss children's story, "The Cat in the Hat."
+     * When the cat in the hat says something, the things, children & fish respond.
+     * In a procedural approach, the Subject class (the cat in the hat) has to know
+     * about all the other classes. When they change, Subject has to be recompiled.
+     * In addition, Subject is managing multiple objects, of multiple classes, with
+     * multiple interfaces, which leads to long, hard to follow code.
+     * Use the Observer pattern to decouple the Subject class from the others.
+     */
 
-namespace observer_problem {
+    namespace observer_problem {
 
-class Thing {
-	string name;
-public:
-	Thing(string name) : name(name) {}
-	~Thing() { cout << "  ~Thing" << name << " leaves.\n"; }
-public:
-	void makesMischief() { cout << "  Thing" << name << " makes mischief.\n"; }
-};
+        class Thing {
+            string name;
+        public:
+            Thing(string name) : name(name) {}
+            ~Thing() { cout << "  ~Thing" << name << " leaves.\n"; }
+        public:
+            void makesMischief() { cout << "  Thing" << name << " makes mischief.\n"; }
+        };
 
-class Fish {
-public:
-	Fish() {}
-	~Fish() { cout << "  ~Fish.\n"; }
-public:
-	void complains() { cout << "  Fish complains.\n"; }
-};
+        class Fish {
+        public:
+            Fish() {}
+            ~Fish() { cout << "  ~Fish.\n"; }
+        public:
+            void complains() { cout << "  Fish complains.\n"; }
+        };
 
-class Child {
-	string gender;
-public:
-	Child(string gender) : gender(gender) {}
-	~Child() { cout << "  ~Child(" << gender << ").\n"; }
-public:
-	void hasFun() { cout << "  " << gender << " has fun.\n"; }
-};
+        class Child {
+            string gender;
+        public:
+            Child(string gender) : gender(gender) {}
+            ~Child() { cout << "  ~Child(" << gender << ").\n"; }
+        public:
+            void hasFun() { cout << "  " << gender << " has fun.\n"; }
+        };
 
-class Mom {
-public:
-	Mom() {}
-	~Mom() { cout << "  ~Mom.\n"; }
-public:
-	void returns() { cout << "  Mom returns.\n"; }
-};
+        class Mom {
+        public:
+            Mom() {}
+            ~Mom() { cout << "  ~Mom.\n"; }
+        public:
+            void returns() { cout << "  Mom returns.\n"; }
+        };
 
-class Subject {	// Has to know about all the objects that are 'listening'.
-	Thing*	thing1;
-	Thing*	thing2;
-	Fish*	fish;
-	Child*	boy;
-	Child*	girl;
-	Mom*	mom;
-	// Seam 1 - insert new objects.
-public:
-	Subject() :
-		thing1(new Thing("1")),
-		thing2(new Thing("2")),
-		fish(new Fish),
-		boy(new Child("Boy")),
-		girl(new Child("Girl")),
-		mom(new Mom)
-		// Seam 2 - insert new objects.
-		{}
-	~Subject() {
-		cout << "  Cat in the hat left: ~Subject.\n";
-		cout << endl;
-	}
-public:
-	void runSim() {
-		// Event 1.
-		cout << "  Cat in the Hat says \"Hello\".\n";
-		thing1->makesMischief();
-		thing2->makesMischief();
-		boy->hasFun();
-		girl->hasFun();
-		cout << endl;
+        class Subject {	// Has to know about all the objects that are 'listening'.
+            Thing*	thing1;
+            Thing*	thing2;
+            Fish*	fish;
+            Child*	boy;
+            Child*	girl;
+            Mom*	mom;
+            // Seam 1 - insert new objects.
+        public:
+            Subject() :
+                thing1(new Thing("1")),
+                thing2(new Thing("2")),
+                fish(new Fish),
+                boy(new Child("Boy")),
+                girl(new Child("Girl")),
+                mom(new Mom)
+                // Seam 2 - insert new objects.
+            {}
+            ~Subject() {
+                cout << "  Cat in the hat left: ~Subject.\n";
+                cout << endl;
+            }
+        public:
+            void runSim() {
+                // Event 1.
+                cout << "  Cat in the Hat says \"Hello\".\n";
+                thing1->makesMischief();
+                thing2->makesMischief();
+                boy->hasFun();
+                girl->hasFun();
+                cout << endl;
 
-		// Event 2.
-		delete thing2;
-		cout << "  Cat in the Hat says \"let's play\".\n";
-		thing1->makesMischief();
-		boy->hasFun();
-		girl->hasFun();
-		cout << endl;
+                // Event 2.
+                delete thing2;
+                cout << "  Cat in the Hat says \"let's play\".\n";
+                thing1->makesMischief();
+                boy->hasFun();
+                girl->hasFun();
+                cout << endl;
 
-		// Event 3.
-		cout << "  Cat in the Hat says \"rock & roll\".\n";
-		thing1->makesMischief();
-		boy->hasFun();
-		girl->hasFun();
-		fish->complains();
-		cout << endl;
+                // Event 3.
+                cout << "  Cat in the Hat says \"rock & roll\".\n";
+                thing1->makesMischief();
+                boy->hasFun();
+                girl->hasFun();
+                fish->complains();
+                cout << endl;
 
-		// Cleanup 1.
-		delete thing1;
-		delete fish;
-		delete girl;
-		delete boy;
+                // Cleanup 1.
+                delete thing1;
+                delete fish;
+                delete girl;
+                delete boy;
 
-		// Event 4.
-		cout << "  Cat in the Hat says \"Goodbye\".\n";
-		mom->returns();
-		cout << endl;
+                // Event 4.
+                cout << "  Cat in the Hat says \"Goodbye\".\n";
+                mom->returns();
+                cout << endl;
 
-		// Cleanup 2.
-		delete mom;
-	}
-};
+                // Cleanup 2.
+                delete mom;
+            }
+        };
 
-void demo() {
-	Subject catInTheHat;
+        void demo() {
+            Subject catInTheHat;
 
-	catInTheHat.runSim();
-}
+            catInTheHat.runSim();
+        }
 
-}
+    }
 
 }
 
