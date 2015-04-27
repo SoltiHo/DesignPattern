19c550d36ad1f6c770dbbe3b726aa50d8a40f81d
Chia-Chiunn Ho (Solti) <Solti.Ho@Gmail.com>
2015-04-26 22:05:47 -0700

    final exam

---
+++ DesignPatternLabs/Final/MyFinal.cpp
@@ -0,0 +1,34 @@
+#include "finalDesign.h"
+
+namespace MyFinalDesign {
+    // order size --> Metals(Aluminum, Stainless steel)
+    // 
+
+
+
+
+
+    class OrderProcessor{
+        void process(map<string, string>& order) {
+            // base info
+            //   - orderNum
+            //   - comment
+            //   - size
+            //   - packager
+            //   - plastic : 4 types
+            //   - moldLoc
+            //   - mold
+            //   - tags
+            //   - color
+
+
+        }
+    };
+
+
+};
+
+int main(){
+    final_design_file::demo("orders_1.txt");
+    return 0;
+}
